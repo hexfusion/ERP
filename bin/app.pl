1b52fb959d9eed01b9d61fb66c35f34b94bdb609
Sam Batschelet <sbatschelet@mac.com>
2015-06-07 15:57:28 -0400

    add proper libs and basic schema configs

--- bin/app.pl
+++ bin/app.pl
@@ -1,7 +1,25 @@
 #!C:\Perl64\bin\perl.exe
 
-use lib '/home/sam/Apps/applications/Angler/lib';
-
+use Plack::Builder;
 use Dancer;
+use Dancer::Plugin::DBIC;
+use Dancer::Handler;
+use lib 'lib';
+#use lib '/home/sam/Apps/angler/applications/Angler/lib';
+use lib "$FindBin::Bin/../../Angler/lib";
 use ERP;
+
+my $app = sub {
+    load_app "ERP";
+    Dancer::App->set_running_app("ERP");
+    my $env = shift;
+    Dancer::Handler->init_request_headers($env);
+    my $req = Dancer::Request->new( env => $env );
+    Dancer->dance($req);
+};
+
+builder {
+    mount "/" => $app;
+};
+
 dance;
