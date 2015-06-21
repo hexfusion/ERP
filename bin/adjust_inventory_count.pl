89e988cd4d509b47fc23d2f39351dd03df74939e
Sam Batschelet <sbatschelet@mac.com>
2015-06-21 07:57:10 -0400

    properly declare vars

--- bin/adjust_inventory_count.pl
+++ bin/adjust_inventory_count.pl
@@ -4,9 +4,9 @@ use warnings;
 use strict;
 use v5.14;
 
+use lib '/home/sam/Apps/angler/applications/Angler/lib';
 use Dancer ':script';
 use Dancer::Plugin::DBIC;
-use Dancer::Plugin::Interchange6;
 use File::Basename;
 use File::Path qw/make_path/;
 use File::Spec;
@@ -19,11 +19,11 @@ use Data::Dumper::Concise;
 set logger => 'console';
 set log    => 'info';
 
-my $schema = shop_schema;
+my $schema = schema('dbic_erp');
 
 my $inventory_report = 
   File::Spec->catfile( [ File::Spec->splitpath($0) ]->[1],
-  'C:', 'home', 'sam', 'Apps', 'data', 'adjust_inventory_count.xlsx' ); 
+  '..', '..', '..', '..', 'data', 'adjust_inventory_count.xlsx' ); 
 
 my $parser = Spreadsheet::ParseXLSX->new;
 
@@ -42,7 +42,7 @@ die "no worksheets found" unless $workbook->worksheet_count;
 my $worksheet = $workbook->worksheet(0);
 die "worksheet not found" unless $worksheet;
 
-my ($item_num_header, $vendor_code_header, $expected_header, $counted_header);
+my ($item_num_header, $vendor_name_header, $expected_header, $counted_header);
 
 # col/row ranges in use
 my ( $row_min, $row_max ) = $worksheet->row_range();
@@ -52,13 +52,13 @@ my ( $col_min, $col_max ) = $worksheet->col_range();
 my $header_col = '6';
 
 foreach my $col ( $col_min .. $col_max ) {
-    my $value = $worksheet->get_cell( $header_col, $col )->value;
+    my $value = $worksheet->get_cell( $row_min, $col )->value;
 
     print "Value" , $value, "\n";
 
     if ( $value eq 'Item #' ) {
-        print "found Item #";
-        $item_num_header = $col;
+       print "found Item #";
+       $item_num_header = $col;
     }
     elsif ( $value eq 'Vendor Name' ) {
         $vendor_name_header = $col;
@@ -71,35 +71,36 @@ foreach my $col ( $col_min .. $col_max ) {
     }
 }
 
-my $vendor_rs = $schema->resultset('Vendor');
+   my $vendor_rs = $schema->resultset('Vendor');
 
-my ( $purchase_order_created, $sales_order_created );
+   my ( $purchase_order_created, $sales_order_created );
 
-while (my $vendor = $vendor_rs->next) {
+   while (my $vendor = $vendor_rs->next) {
     foreach my $row ( $row_min + 1 .. $row_max ) {
+        my ( $item_num, $vendor_name, $expected, $counted );
         my $item_num_col = $worksheet->get_cell( $row, $item_num_header);
         my $vendor_name_col = $worksheet->get_cell( $row, $vendor_name_header );
         my $expected_col = $worksheet->get_cell( $row, $expected_header );
         my $counted_col =  $worksheet->get_cell( $row, $counted_header );
 
         if ( defined  $item_num_col ) {
-            my $item_num =  $item_num_col->value;
+            $item_num =  $item_num_col->value;
         }
         if ( defined  $vendor_name_col ) {
-            my $vendor_name = $vendor_name_col->value;
+            $vendor_name = $vendor_name_col->value;
         }
         if ( defined  $expected_col ) {
-            my $expected =  $expected_col->value;
+            $expected =  $expected_col->value;
         }
         if ( defined  $counted_col ) {
-            my $counted = $counted_col->value;
+            $counted = $counted_col->value;
         }
 
         # check if the vendor in loop is the same as the vendor excel record
         if ( $vendor->companyname eq $vendor_name ) {
             my $differnce = ( $expected - $counted ); 
             my $vendor_listid =  $vendor->listid;
-            my $product = $schema->resultset('PurchaseOrderItem')->find({ itemnumber =>  $item_num });
+            my $product = $schema->resultset('ItemInventory')->find({ itemnumber =>  $item_num });
             die "product not found in system " unless $product;
 
             # if difference is negative we need to create a purchase order
